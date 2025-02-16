068877356f0da6e1c57b38a3e9b4192126248fa2
Sa9ne <trealev@gmail.com>
2025-02-16 17:32:23 +0500

    Fix database error

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"Habit-tracker/internal/models"
 	"Habit-tracker/internal/ui"
+	"fmt"
+	"os"
+	"path/filepath"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -24,9 +27,29 @@ func main() {
 	icons := ui.LoadIcons()
 	MainWindow.SetIcon(icons.MainIcon)
 
+	// Получаем путь к папке "Документы" пользователя
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println("Ошибка получения домашней директории:", err)
+		return
+	}
+
+	dbPath := filepath.Join(homeDir, "Documents", "Habit-tracker")
+	if err := os.MkdirAll(dbPath, os.ModePerm); err != nil {
+		fmt.Println("Ошибка создания директории:", err)
+		return
+	}
+
+	dbFile := filepath.Join(dbPath, "Habits.db")
+
 	// Работа с базой данных
 	var habits []models.Habits
-	DB, _ := gorm.Open(sqlite.Open("Habits.db"), &gorm.Config{})
+	DB, err := gorm.Open(sqlite.Open(dbFile), &gorm.Config{})
+	if err != nil {
+		fmt.Println("Ошибка подключения к базе данных:", err)
+		return
+	}
+
 	DB.AutoMigrate(&models.Habits{})
 	DB.Find(&habits)
 
